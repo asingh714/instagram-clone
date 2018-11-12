05af3f0858e1982e0ab665be85092cc0f947fe3b
Aman <amsingh714@gmail.com>
2018-11-12 18:31:10 -0500

    added searchbar with minor styling

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import './SearchBar.css'
 
 
 const SearchBar = () => {
+    return (
+        <div className="SearchBar">
+        <div className="logo-name-section">
+            <i className="fab fa-instagram"></i>
+            <h1>Instagram</h1>
+        </div>
+        <div className="search-section">
+        <input type="text" className="fa fa-input" placeholder=" Search" />
+        </div>
+        <div className="nav-section">
+            <i className="far fa-compass"></i>
+            <i className="far fa-heart"></i>
+            <i className="far fa-user"></i>
+        </div>
 
+        </div>
+    );
 }
 
 export default SearchBar;
