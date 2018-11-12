bdbba384362ac745642d03bd6e8eb397ae9792ac
Aman <amsingh714@gmail.com>
2018-11-12 18:09:20 -0500

    added component files and dummy data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import SearchBar from './components/SearchBar'
+
+import dummyData from './dummy-data'
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: dummyData
+    }
+  }
+
+
   render() {
     return (
       <div className="App">
-       <h1>Instagram</h1>
+       <SearchBar /> 
       </div>
     );
   }
