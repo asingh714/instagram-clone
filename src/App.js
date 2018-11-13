e3e98dcf3b892849f83216bb2a5890085b0ab20e
Aman <amsingh714@gmail.com>
2018-11-12 20:02:42 -0500

    made component folders

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import SearchBar from './components/SearchBar'
-import PostContainer from './components/PostContainer'
+import SearchBar from './components/SearchBar/SearchBar'
+import PostContainer from './components/PostContainer/PostContainer'
 
 import dummyData from './dummy-data'
 import './App.css';
