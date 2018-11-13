7434d7ae7be0d41a2e41e2d2e6e563955ddac0de
Aman <amsingh714@gmail.com>
2018-11-12 19:10:56 -0500

    added proptypes to postcontainer

--- src/components/PostContainer.js
+++ src/components/PostContainer.js
@@ -1,22 +1,33 @@
-import React from 'react';
-import CommentSection from './CommentSection'
+import React from "react";
+import CommentSection from "./CommentSection";
+import PropTypes from "prop-types";
 
 const PostContainer = props => {
-    return (
+  return (
     <div>
-        {props.postData.map(post => 
-            <div>
-                <img src={post.thumbnailUrl} alt="Thumbnail Image" />,
-                <p>{post.username}</p>
-                <img src={post.imageUrl} alt="Posted Image" />
-                <p>{post.likes}</p>
-                <p>{post.timestamp}</p>
-            </div>
-        )};
-        <CommentSection />
+      {props.postData.map(post => (
+        <div>
+          <img src={post.thumbnailUrl} alt="Thumbnail Image" />,
+          <p>{post.username}</p>
+          <img src={post.imageUrl} alt="Posted Image" />
+          <p>{post.likes}</p>
+          <p>{post.timestamp}</p>
+        </div>
+      ))}
+      ;
+      <CommentSection postComments={props.postData.comments} />
     </div>
+  );
+};
 
-    )
-}
+PostContainer.propTypes = {
+  postData: PropTypes.arrayOf(
+    PropTypes.shape({
+      username: PropTypes.string,
+      likes: PropTypes.number,
+      timestamp: PropTypes.string
+    })
+  )
+};
 
 export default PostContainer;
