53855de26095f09a308f1b65bf38b78e06551360
Aman <amsingh714@gmail.com>
2018-11-13 21:01:14 -0500

    added styles to top half of post

--- src/components/PostContainer/Post.js
+++ src/components/PostContainer/Post.js
@@ -1,15 +1,18 @@
 import React from "react";
 import CommentSection from "../CommentSection/CommentSection";
 import PropTypes from "prop-types";
+import './Post.css';
 
 const Post = props => {
   return (
     <div className="post-section">
       <div className="top-post-section">
         <img src={props.post.thumbnailUrl} alt="Thumbnail Image" />
-        <p>{props.post.username}</p>
+        <p className="bold-text">{props.post.username}</p>
       </div>
-      <img src={props.post.imageUrl} alt="Posted Image" />
+      <div className="post-img">
+        <img src={props.post.imageUrl} alt="Posted Image" />
+      </div>
       <CommentSection comments={props.post.comments} timestamp={props.post.timestamp} likes={props.post.likes}/>
     </div>
   );
