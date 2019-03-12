deb4cc27f47884c6632a12134ac8d627edfd0fd0
Aman <amsingh714@gmail.com>
2019-03-12 11:08:29 -0400

    refactored code for comment css and js

--- src/CommentSection/Comment.js
+++ src/CommentSection/Comment.js
@@ -1,24 +1,26 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from 'styled-components';
-import Username from '../../Styles/Reusables'
-import './Comment.css'
 
-const UserComments = styled.div`
+// import "./Comment.css"
+
+export const CommentContainer = styled.div`
   display: flex;
   margin-bottom: .5rem;
 `
 
+export const Username = styled.p`
+  font-weight: bold;
+  margin-right: .5rem;
+`
+
+
 const Comment = props => {
   return (
-    <UserComments>
-      {/* Now we can pass the comment that is from
-      CommentSection and use it here for username and 
-      text.
-      */}
-        <Username type="space">{props.comment.username}</Username>
-        <p>{props.comment.text}</p>
-    </UserComments>
+    <CommentContainer>
+      <Username>{props.comment.username}</Username>
+      <p>{props.comment.text}</p>
+    </CommentContainer>
   );
 };
 
@@ -27,6 +29,6 @@ Comment.propTypes = {
     username: PropTypes.string,
     text: PropTypes.string,
   })
-};
+}
 
 export default Comment;
