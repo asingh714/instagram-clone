066929f543426ce4c7cd54f2ea97781f37ebb973
Aman <amsingh714@gmail.com>
2019-03-12 11:08:56 -0400

    refactored code for CommentForm

--- src/CommentSection/CommentForm.js
+++ src/CommentSection/CommentForm.js
@@ -1,31 +1,29 @@
 import React from "react";
-import styled from "styled-components";
-import "./CommentForm.css";
+import styled from 'styled-components';
 
-const CommentBox = styled.input`
-  width: 100%;
-  padding: 1.5rem 1rem;
-  border: none;
-  border-top: 1px solid #eee;
-  outline: none;
-`;
+// import "./CommentForm.css"
+
+export const CommentInput = styled.input`
+width: 100%;
+padding: 1.5rem 1rem;
+border: none;
+border-top: 1px solid #eee;
+outline: none;
+font-size: 1.4rem;
+}
+`
 
 const CommentForm = props => {
-  return (
-    /* 
-      - onSubmit will allow us to press enter and submit a 
-      comment. 
-      - the value of what is being typed in the comment box
-      will be saved as value.
-      - 
-    */
-    <form onSubmit={props.addComment}>
-      <CommentBox
-        type="text"
-        placeholder="Add a comment..."
-        value={props.comment}
-        onChange={props.handleComment}
-      />
+return (
+    <form onSubmit={props.addNewComment}>
+      <CommentInput 
+        className="comment-input" 
+        type="text" 
+        placeholder="Add a comment..." 
+        value={props.comment} 
+        name="comment"
+        onChange={props.handleChange}
+        />
     </form>
   );
 };
