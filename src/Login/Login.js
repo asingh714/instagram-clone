6b5f9b3f5162b4b64e572af7d29cd8a9cc6ee912
Aman <amsingh714@gmail.com>
2019-03-12 11:07:36 -0400

    refactored code for Login js and css

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,46 +1,38 @@
-import React, { Component } from "react";
+import React from "react";
+
 import styled from 'styled-components';
-import './Login.css'
-import IGLogo from '../../Assets/iglogo.png';
+import IGLogo from "../Assets/iglogo.png"
+// import "./Login.css"
 
-/**
-1. Create login directory
-2. Add login.js to directory
-3. Create username input, password input
-4. Add the login component to Authenticate.js
-5. Need to create logic for setting username on localstorage. 
-6. Create login function that sets username on localStorage
-7. Force page to re-load.
-*/
-
-const LoginBox = styled.div`
+export const LoginPageContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   height: 100vh;
+  background-color: #FAFAFA;
 `
-const Form = styled.form`
+
+export const LoginFormContainer = styled.form`
   background-color: white;
   display: flex;
   flex-direction: column;
   padding: 6rem 10rem;
   width: 50rem;
+  border: 1px solid #ddd;
 `
-
-
-const LogoImage = styled.img`
+export const LoginImg = styled.img`
   margin: 0 auto;
   margin-bottom: 1rem;
   width: 20rem;
-`;
+`
 
-const Input = styled.input`
+export const LoginInput = styled.input`
   padding: 1rem;
   margin-bottom: 1rem;
   outline: none;
 `
 
-const Button = styled.div`
+export const LoginButton = styled.button`
   color: white;
   background-color: #3e99ed;
   cursor: pointer;
@@ -48,49 +40,47 @@ const Button = styled.div`
   text-align: center
 `
 
-class Login extends Component {
-  constructor() {
-    super();
-    this.state = {
-      username: "",
-      password: ""
-    };
-  }
 
-  handleInputChange = event => {
-    this.setState({
-      [event.target.name]: [event.target.value]
-    });
+class Login extends React.Component {
+  state = {
+    username: "",
+    password: ""
   };
 
   handleLogin = event => {
-    const user = this.state.username;
-    localStorage.setItem("user", user);
+    event.preventDefault();
+    localStorage.setItem("user", this.state.username);
     window.location.reload();
+  };
+
+  handleInputChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
   };
 
   render() {
     return (
-      <LoginBox>
-        <Form onSubmit={this.handleLogin}>
-        <LogoImage src={IGLogo} alt="instagram logo" />
-          <Input
+      <LoginPageContainer>
+        <LoginFormContainer onSubmit={this.handleLogin}>
+        <LoginImg src={IGLogo} alt="Instagram Logo"/>
+          <LoginInput
             type="text"
             placeholder="Username"
-            name="username"
             value={this.state.username}
+            name="username"
             onChange={this.handleInputChange}
           />
-          <Input
-            type="text"
+          <LoginInput
+            type="password"
             placeholder="Password"
-            name="password"
             value={this.state.password}
+            name="password"
             onChange={this.handleInputChange}
           />
-          <Button onClick={this.handleLogin}>Log in</Button>
-        </Form>
-      </LoginBox>
+          <LoginButton>Log In</LoginButton>
+        </LoginFormContainer>
+      </LoginPageContainer>
     );
   }
 }
